dcf2ad13f2b629779eeaab7887ed0d84fa1f87e5
cleverbao <cleverbao@outlook.com>
2019-05-10 11:59:51 +0800

    处理站点不更新问题
    集合高德地图api,处理北京公交官网始发站站点不更新问题.
    适配微信小程序的云数据库格式.
    使用时需要自行注册高德地图api,填写对应的key.

--- 北京公交路线信息/getBjBus.py
+++ 北京公交路线信息/getBjBus.py
@@ -17,9 +17,9 @@ logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s',datefmt = '%Y-%m-%d %H:%M:%S'
 )
 # 使用FileHandler输出到文件
-File_log = logging.FileHandler('GetBjBus.log', 'w')
-File_log.setLevel(logging.INFO)
-File_log.setFormatter(formatter)
+# File_log = logging.FileHandler('GetBjBus.log', 'w')
+# File_log.setLevel(logging.INFO)
+# File_log.setFormatter(formatter)
 
 # 使用StreamHandler输出到屏幕
 Terminal_log = logging.StreamHandler()
@@ -28,7 +28,7 @@ Terminal_log.setFormatter(formatter)
 
 # 添加两个Handler
 logger.addHandler(Terminal_log)
-logger.addHandler(File_log)
+# logger.addHandler(File_log)
 
 
 RequestHeaders = {
@@ -37,6 +37,8 @@ RequestHeaders = {
     }
 logger.info('初始化session!')
 session = requests.Session()
+
+Amap_Key = '高德api key'
 
 def GetAllBusLineName():
     Data = []
@@ -107,10 +109,15 @@ def GetAllLineDirectionStation(LineDirectionValue):
 
     DataLineDirectionStation = re.findall(Reg, r.text)
 
-    TmpData = []
+    TmpData = {}
+    i = 0
     for key in DataLineDirectionStation:
-        TmpData.append(key)
-    Data['station'] = TmpData
+        TmpData[i] = key
+        i += 1
+
+    Data['Stations'] = TmpData
+    if TmpData:
+        Data['MidStation'] = TmpData[len(TmpData)//2]
     
     return Data
 
@@ -129,32 +136,72 @@ def GetAllLineDirectionMsg(LineDirectionValue):
 
 
     Reg = r'<p>.*?&nbsp;(.*?)&nbsp;'
+    try:
+        DataLineDirectionMsg = re.findall(Reg, r.json()['html'])
+        for key in DataLineDirectionMsg:
+            Data['time'] = key
+    except Exception as e : 
+        Data['time'] = ''
 
-    DataLineDirectionMsg = re.findall(Reg, r.json()['html'])
-
-    for key in DataLineDirectionMsg:
-        Data['time'] = key
-    
     return Data
 
+
+def ResolveError(LineData):
+
+    LineName = LineData['LineName']
+    StartStation = LineData['StartStation']
+    EndStation = LineData['EndStation']
+
+    Reg = r'^(.*?)\((.*?)-(.*?)\)$'
+    SearchResult = re.search(Reg, LineName)
+    if SearchResult:
+        SearchLineName = SearchResult.group(1)
+    else:
+        return False
+
+    url = 'https://restapi.amap.com/v3/bus/linename?s=rsv3&city=010&citylimit=true&extensions=all&output=json&city=010&offset=2&key={}&keywords={}路'.format(Amap_Key, SearchLineName)
+
+    logger.info('发送一个http请求,地址{}'.format(url))
+
+    r = session.get(
+        url = url
+        )
+    r.encoding = r.apparent_encoding
+
+    AmapData = r.json()
+    for busline in AmapData['buslines']:
+        if busline['start_stop'] == StartStation or busline['end_stop'] == EndStation:
+            LineData['StartStation'] = busline['start_stop']
+            LineData['EndStation'] = busline['end_stop']
+            LineData['LineName'] = busline['name']
+
+    return LineData
+
+
 if __name__ == "__main__" :
-    Data = []
     logger.info('获取所有的路线名称!')
+    file = open('BjBus.json', 'w')
+
     for LineName in GetAllBusLineName():
         logger.info('获取路线{}的方向名称!'.format(LineName))
         for LineDirection in GetAllBusLineDirection(LineName):
-            TmpData = []
-            TmpData.append(LineDirection)
+            Data = {}
+            Data.update(LineDirection)
+
             logger.info('获取路线{}的站点!'.format(LineDirection['LineName']))
             LineDirectionStation =  GetAllLineDirectionStation(LineDirection['Value'])
-            TmpData.append(LineDirectionStation)
+
+            Data.update(LineDirectionStation)
             logger.info('获取路线{}的时间!'.format(LineDirection['LineName']))
             LineDirectionMsg = GetAllLineDirectionMsg(LineDirection['Value'])
-            TmpData.append(LineDirectionMsg)
-            Data.append(TmpData)
-    
-    print(Data)
 
-    with open('BjBus.json', 'w') as f:
-        json.dump(Data, f)
+            Data.update(LineDirectionMsg)
+            Data = ResolveError(Data)
+
+            print(Data)
+
+            json.dump(Data, file)
+            file.write('\n')
+        
+    file.close()
 
